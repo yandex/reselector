a474d1358cbfc98334a3c114069d259d2dd9a7b6
lttb <kenzhaev.artur@gmail.com>
2018-03-07 15:19:59 +0300

    Fix config usage, add prefix option

---
+++ src/config.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const cosmiconfig = require('cosmiconfig')
+const { name } = require('../package.json')
+
+const { config } = (cosmiconfig(name, { sync: true }).load() || {})
+
+module.exports = Object.assign({
+  prefix: 'data-',
+  syntaxes: [
+    '@babel/plugin-syntax-async-generators',
+    '@babel/plugin-syntax-class-properties',
+    '@babel/plugin-syntax-decorators',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-export-default-from',
+    '@babel/plugin-syntax-export-namespace-from',
+    '@babel/plugin-syntax-flow',
+    '@babel/plugin-syntax-function-bind',
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-syntax-jsx',
+    '@babel/plugin-syntax-nullish-coalescing-operator',
+    '@babel/plugin-syntax-numeric-separator',
+    '@babel/plugin-syntax-object-rest-spread',
+    '@babel/plugin-syntax-optional-catch-binding',
+    '@babel/plugin-syntax-optional-chaining',
+    '@babel/plugin-syntax-pipeline-operator',
+    '@babel/plugin-syntax-throw-expressions',
+  ],
+}, config)

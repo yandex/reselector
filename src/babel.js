ca39ae17d5b8906f527b799dae591441509ea37d
nickshevr <nickshevr@yandex-team.ru>
2018-04-23 15:59:16 +0300

    Fix for conditional render component

--- src/babel.js
+++ src/babel.js
@@ -1,10 +1,13 @@
 'use strict'
 
 const template = require('@babel/template').default
+const hash = require('string-hash')
 
 const config = require('./config')
 const { TEST_ID } = require('./const')
 const { getName, getId, getNode } = require('./utils')
+
+const attributeMap = {}
 
 const build = template(`
   COMPONENT.PROP = ID
@@ -53,6 +56,17 @@ module.exports = ({ types: t }) => {
         ) : (
           t.JSXAttribute(t.JSXIdentifier(propName))
         )
+
+        if (prop.type === 'JSXAttribute') {
+          const propMeta = !prop ? 'null' : hash(JSON.stringify(prop))
+          const hashId = hash(`${filename}_${name}_${propMeta}`).toString(16)
+
+          if (attributeMap[hashId]) {
+            return
+          }
+
+          attributeMap[hashId] = true
+        }
 
         openingElement.attributes.push(prop)
 
